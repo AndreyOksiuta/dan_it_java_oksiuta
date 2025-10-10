1f78e9a0418e33c6e7d6700c2c78060879d6bf21
Andrii Oksiuta <andreyka2011ua@gmail.com>
2025-10-10 11:47:44 +0200

    first lessons with variables

---
+++ java_developmen/src/main/java/first_lessons/Variables.java
@@ -0,0 +1,19 @@
+package first_lessons;
+
+public class Variables {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println(a+b);
+        byte byteVariables=1;
+        System.out.println(byteVariables);
+        String str="1";
+        System.out.println(str+1);
+        System.out.println("1"+1+1);
+        System.out.println(1+1+"1");
+        char charVariables=123;
+        System.out.println(charVariables);
+        char charVariables2='c';
+        System.out.println((int) charVariables2);
+    }
+}
