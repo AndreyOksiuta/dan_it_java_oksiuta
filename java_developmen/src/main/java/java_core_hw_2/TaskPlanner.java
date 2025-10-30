c0a1ab565202a8eb738b602d1085bf424f2bb7d8
Andrii Oksiuta <andreyka2011ua@gmail.com>
2025-10-30 18:52:49 +0100

    change

--- java_developmen/src/main/java/java_core_hw_2/TaskPlanner.java
+++ java_developmen/src/main/java/java_core_hw_2/TaskPlanner.java
@@ -5,56 +5,43 @@ import java.util.Scanner;
 public class TaskPlanner {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String[][] scedule = new String[7][2];
-        scedule[0][0] = "sunday";
-        scedule[0][1] = "chiil";
-        scedule[1][0] = "monday";
-        scedule[1][1] = "Go to mall";
-        scedule[2][0] = "tuesday";
-        scedule[2][1] = "Go to gym";
-        scedule[3][0] = "wednesday";
-        scedule[3][1] = "Go to pool";
-        scedule[4][0] = "thursday";
-        scedule[4][1] = "go to courses; watch a film";
-        scedule[5][0] = "friday";
-        scedule[5][1] = "Go to bar";
-        scedule[6][0] = "saturday";
-        scedule[6][1] = "Go to parents";
+        String[][] schedule = new String[7][2];
+        newSchedule(schedule);
         while (true) {
             System.out.println("Please, input the day of the week");
-            String day = input.nextLine();
-            String[] days = day.split(" ");
+            String dayOfWeek = input.nextLine();
+            String[] days = dayOfWeek.split(" ");
             switch (days[0].trim().toLowerCase()) {
                 case "sunday":
-                    System.out.println(scedule[0][1]);
+                    System.out.println(schedule[0][1]);
                     break;
                 case "monday":
-                    System.out.println(scedule[1][1]);
+                    System.out.println(schedule[1][1]);
                     break;
                 case "tuesday":
-                    System.out.println(scedule[2][1]);
+                    System.out.println(schedule[2][1]);
                     break;
                 case "wednesday":
-                    System.out.println(scedule[3][1]);
+                    System.out.println(schedule[3][1]);
                     break;
                 case "thursday":
-                    System.out.println(scedule[4][1]);
+                    System.out.println(schedule[4][1]);
                     break;
                 case "friday":
-                    System.out.println(scedule[5][1]);
+                    System.out.println(schedule[5][1]);
                     break;
                 case "saturday":
-                    System.out.println(scedule[6][1]);
+                    System.out.println(schedule[6][1]);
                     break;
                 case "exit":
                     return;
                 case "change":
                     boolean findDay = false;
-                    for (int i = 0; i < 7; i++) {
-                        if (days[1].equalsIgnoreCase(scedule[i][0])) {
-                            System.out.println("Please, input new tasks for " + scedule[i][0]);
+                    for (int i = 0; i < schedule.length; i++) {
+                        if (days[1].equalsIgnoreCase(schedule[i][0])) {
+                            System.out.println("Please, input new tasks for " + schedule[i][0]);
                             String data = input.nextLine();
-                            scedule[i][1] = data;
+                            schedule[i][1] = data;
                             findDay = true;
                             break;
                         }
@@ -68,5 +55,21 @@ public class TaskPlanner {
                     break;
             }
         }
+    }
+    private static void newSchedule(String[][] schedule) {
+        schedule[0][0] = "sunday";
+        schedule[0][1] = "chiil";
+        schedule[1][0] = "monday";
+        schedule[1][1] = "Go to mall";
+        schedule[2][0] = "tuesday";
+        schedule[2][1] = "Go to gym";
+        schedule[3][0] = "wednesday";
+        schedule[3][1] = "Go to pool";
+        schedule[4][0] = "thursday";
+        schedule[4][1] = "go to courses; watch a film";
+        schedule[5][0] = "friday";
+        schedule[5][1] = "Go to bar";
+        schedule[6][0] = "saturday";
+        schedule[6][1] = "Go to parents";
     }
 }
